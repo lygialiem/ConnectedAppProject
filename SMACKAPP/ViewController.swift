9c4d105633cb6f42772feeb2c456ae6ee4890448
Lý Gia Liêm <LGL@MacBook-Pro-cua-Liem.local>
2019-04-22 19:03:07 +0700

    Setup Project

---
+++ SMACKAPP/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SMACKAPP
+//
+//  Created by Lý Gia Liêm on 4/22/19.
+//  Copyright © 2019 LGL. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
